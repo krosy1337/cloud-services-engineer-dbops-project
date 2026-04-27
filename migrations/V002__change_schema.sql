42721c0e734c9983c1ab5f38b5f139a9ca7c13b0
Alexey Egorov <krosy1337@gmail.com>
2026-04-27 19:05:22 +0300

    фикс

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -1,12 +1,15 @@
 DELETE FROM order_product
 WHERE
-    order_id NOT IN (SELECT id FROM orders) OR product_id NOT IN (SELECT id FROM product); 
+    order_id NOT IN (SELECT id FROM orders) OR product_id NOT IN (SELECT id FROM product);
+
+CREATE UNIQUE INDEX idx_product_id ON product(id); 
 
 ALTER TABLE order_product
     ADD CONSTRAINT fk_order_product_product
     FOREIGN KEY (product_id)
     REFERENCES product (id);
 
+CREATE UNIQUE INDEX idx_order_id ON orders(id); 
 
 ALTER TABLE order_product
     ADD CONSTRAINT fk_order_product_orders
