6ef9d8cf1f0e3658ee0ccd3961d498cb038e88e6
Alexey Egorov <krosy1337@gmail.com>
2026-04-27 20:43:13 +0300

    может быть фикс

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -1,23 +1,14 @@
-DELETE FROM order_product
-WHERE
-    order_id NOT IN (SELECT id FROM orders) OR product_id NOT IN (SELECT id FROM product);
+ALTER TABLE product
+    ADD price DOUBLE PRECISION,
+    ADD PRIMARY KEY (id);
 
-CREATE UNIQUE INDEX idx_product_id ON product(id); 
+ALTER TABLE orders
+    ADD date_created DATE,
+    ADD PRIMARY KEY (id);
 
-ALTER TABLE order_product
-    ADD CONSTRAINT fk_order_product_product
-    FOREIGN KEY (product_id)
-    REFERENCES product (id);
-
-CREATE UNIQUE INDEX idx_order_id ON orders(id); 
+DROP TABLE product_info;
+DROP TABLE orders_date;
 
 ALTER TABLE order_product
-    ADD CONSTRAINT fk_order_product_orders
-    FOREIGN KEY (order_id)
-    REFERENCES orders (id);
-
-ALTER TABLE product ADD COLUMN price double precision;
-ALTER TABLE orders ADD COLUMN date_created date default current_date;
-
-DROP TABLE IF EXISTS orders_date;
-DROP TABLE IF EXISTS product_info;
+    ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES product (id),
+    ADD CONSTRAINT fk_orders_id FOREIGN KEY (order_id) REFERENCES orders (id);
