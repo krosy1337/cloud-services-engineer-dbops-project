6c4ebc0c339311c2eec81a3c738140b22c34f505
Alexey Egorov <krosy1337@gmail.com>
2026-04-27 19:56:21 +0300

    уменьшил количество данных, а то не может завершиться миграция

--- migrations/V003__insert_data.sql
+++ migrations/V003__insert_data.sql
@@ -17,7 +17,7 @@ SELECT
     (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)],
     DATE(NOW() - (random() * (NOW()+'90 days' - NOW())))
 FROM
-    generate_series(1, 10000000) s(i);
+    generate_series(1, 100000) s(i);
 
 INSERT INTO order_product (quantity, order_id, product_id)
 SELECT
@@ -25,4 +25,4 @@ SELECT
     i,
     1 + floor(random()*6)::int % 6
 FROM
-    generate_series(1, 10000000) s(i);
+    generate_series(1, 100000) s(i);
